ba2b411ae070a61905e3160f4bdfd353a22ec0ff
ZenoCoding <tychoyoung@gmail.com>
2025-01-14 21:20:29 -0800

    typo

--- src/main/java/com/team9470/subsystems/AlgaeArm.java
+++ src/main/java/com/team9470/subsystems/AlgaeArm.java
@@ -1,5 +1,7 @@
 package com.team9470.subsystems;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 /**
  * AlgaeArm - Neev + Aadit
  * Subsystem Requirements:
@@ -11,5 +13,5 @@ package com.team9470.subsystems;
  * - one arm motor, one wheel motor
  * - motion magicc
  */
-public class AlgaeArm {
+public class AlgaeArm extends SubsystemBase {
 }

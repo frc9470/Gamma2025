5950da6971ea965b1afd80b48ba4dec62e3129ea
Aadit Bansal <aaditbansal@aadits-air.lan>
2025-01-19 16:44:02 -0800

    detect algae + fix coral detection

--- src/main/java/com/team9470/subsystems/AlgaeArm.java
+++ src/main/java/com/team9470/subsystems/AlgaeArm.java
@@ -2,42 +2,44 @@ package com.team9470.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.math.controller.ArmFeedforward;
 
 /**
  * AlgaeArm - Neev + Aadit
  * Subsystem Requirements:
  * - move arm up or down with command - DONE
- * - use a sensor to detect if the arm is at the top or bottom - KINDA DONE
- * - detect if the arm is holding algae with current sensor - IDK
- * - detect if the arm is in the way of the CoralManipulator - KINDA DONE
+ * - use a sensor to detect if the arm is at the top or bottom - DONE
+ * - detect if the arm is holding algae with current sensor - DONE
+ * - detect if the arm is in the way of the CoralManipulator - DONE
  * - roll algae wheels to manipulate algae - DONE
  * - one arm motor, one wheel motor - DONE
  * - motion magicc - IDK
  */
 public class AlgaeArm extends SubsystemBase {
+    // constants (random values for now)
     final double KS = 0;
     final double KG = 0;
     final double KV = 0;
-
-    final double CORAL_MIN_POS = Math.toRadians(-45);
-    final CoralManipulator coral;
-
+    final double CORAL_THRESHOLD = Math.toRadians(45);
+    final double ALGAE_IN_THRESHOLD = 0;
     final double ANGLE_UP = Math.toRadians(45);
     final double ANGLE_DOWN = Math.toRadians(45);
+    final int ARM_MOTOR_ID = 0;
+    final int ROLLER_MOTOR_ID = 1;
 
 
     private double setpoint = 0;
     private int onTop = 0; // top = 1; bottom = -1
-    private TalonFX motor = new TalonFX(0);
-    private TalonFX rollers = new TalonFX(1);
+    private TalonFX motor = new TalonFX(ARM_MOTOR_ID);
+    private TalonFX rollers = new TalonFX(ROLLER_MOTOR_ID);
     private ArmFeedforward ff = new ArmFeedforward(KS, KG, KV);
 
     public AlgaeArm(CoralManipulator coral){
-        this.coral = coral;
     }
 
     public void periodic(){
@@ -62,14 +64,18 @@ public class AlgaeArm extends SubsystemBase {
         rollers.set(speed);
     }
 
-    public StatusSignal<Angle> getEncoderPosition(){
-        return motor.getPosition();
+    public Angle getAngle(){
+        return motor.getPosition().getValue();
+    }
+    public Current getRollerCurrent(){
+        return rollers.getStatorCurrent().getValue();
     }
 
-    public boolean detectCoralManipulator(){
-        if(coral.getEncoderPosition().getValueAsDouble() <= CORAL_MIN_POS){
-            return true;
-        }
-        return false;
+    public boolean blockingCoralManipulator(){
+        return getAngle().gte(Units.Degrees.of(CORAL_THRESHOLD));
+    }
+
+    public boolean AlgaeIn(){
+        return getRollerCurrent().gte(Units.Amps.of(ALGAE_IN_THRESHOLD));
     }
 }

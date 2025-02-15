8b215c71ba6852b94937c886be056f8bc9eff9f5
ZenoCoding <tychoyoung@gmail.com>
2025-02-15 15:26:15 -0800

    robotcontainer keybindings update

--- src/main/java/com/team9470/RobotContainer.java
+++ src/main/java/com/team9470/RobotContainer.java
@@ -14,16 +14,13 @@ import com.team9470.subsystems.CoralManipulator;
 import com.team9470.subsystems.Elevator;
 import com.team9470.subsystems.Swerve;
 import com.team9470.subsystems.vision.Vision;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
 import static edu.wpi.first.units.Units.*;
 
@@ -90,14 +87,13 @@ public class RobotContainer {
         );
 
 //        xbox.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        xbox.b().whileTrue(new InstantCommand(() -> drivetrain.resetRotation(Rotation2d.fromDegrees(0))));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
-        xbox.back().and(xbox.y()).whileTrue(drivetrain.sysIdDynamic(SysIdRoutine.Direction.kForward));
-        xbox.back().and(xbox.x()).whileTrue(drivetrain.sysIdDynamic(SysIdRoutine.Direction.kReverse));
-        xbox.start().and(xbox.y()).whileTrue(drivetrain.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-        xbox.start().and(xbox.x()).whileTrue(drivetrain.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+//        xbox.back().and(xbox.y()).whileTrue(drivetrain.sysIdDynamic(SysIdRoutine.Direction.kForward));
+//        xbox.back().and(xbox.x()).whileTrue(drivetrain.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+//        xbox.start().and(xbox.y()).whileTrue(drivetrain.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+//        xbox.start().and(xbox.x()).whileTrue(drivetrain.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
 
 
         xbox.leftBumper().whileTrue(elevator.L3().andThen(alg.deploy().alongWith(alg.spin()))).onFalse(alg.stow().andThen(elevator.L0()));
@@ -105,10 +101,6 @@ public class RobotContainer {
 
         // reset the field-centric heading on left bumper press
         xbox.b().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
-
-        // coral intake
-        xbox.rightBumper().whileTrue(coral.scoreCommand());
-        xbox.leftBumper().whileTrue(coral.reverseCommand());
 
         drivetrain.registerTelemetry(logger::telemeterize);
 

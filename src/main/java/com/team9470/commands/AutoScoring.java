4ced28dd257fe41363acac2053213e7067a70a9d
Aadit Bansal <aaditbansal@aadits-air.lan>
2025-03-09 12:52:39 -0700

    add coral scoring logic with coral tracking

--- src/main/java/com/team9470/commands/AutoScoring.java
+++ src/main/java/com/team9470/commands/AutoScoring.java
@@ -16,20 +16,21 @@ import static edu.wpi.first.units.Units.Meters;
 
 public class AutoScoring {
 
-    private CoralObjective coralObjective = AutoScoring.CoralObjective.NONE;
-    private final Swerve drivetrain;
+    private ScoringState scoringState = ScoringState.DEFAULT;
+    private static Swerve drivetrain;
 
     public AutoScoring(Swerve drivetrain){
         this.drivetrain = drivetrain;
     }
 
     public Command autoScore(Superstructure superstructure) {
+        CoralObjective coralObjective = scoringState.getOptimalObjective();
         return new DeferredCommand(() -> {
             Command driveToScore = new DriveToPose(coralObjective::getScoringPose, drivetrain)
                     .alongWith(
                             new WaitUntilCommand(() -> closeEnough(coralObjective, Constants.DriverAssistConstants.RAISE_DISTANCE))
                                     .andThen(superstructure.waitForIntake().asProxy())
-                                    .andThen(superstructure.raise(coralObjective.level).asProxy())
+                                    .andThen(superstructure.raise(scoringState.getLevel()).asProxy())
                     );
             System.out.println("Scoring with coralobjective: " + coralObjective);
             return driveToScore.andThen(superstructure.getCoral().scoreCommand().asProxy());
@@ -59,7 +60,7 @@ public class AutoScoring {
         return driveToScore.andThen(superstructure.getCoral().scoreCommand().withTimeout(timeout).asProxy());
     }
     public Command autoScoreNoDrive(Superstructure superstructure) {
-        return new DeferredCommand(() -> superstructure.raise(coralObjective.level), Set.of(superstructure)).andThen(superstructure.score());
+        return new DeferredCommand(() -> superstructure.raise(scoringState.getLevel()), Set.of(superstructure)).andThen(superstructure.score());
     }
 
     private Pose2d computeDriveBackPose(Pose2d scoringPose) {
@@ -69,7 +70,7 @@ public class AutoScoring {
     }
 
     public Command driveBack(){
-        Pose2d scoringPose = coralObjective.getScoringPose();
+        Pose2d scoringPose = scoringState.getOptimalObjective().getScoringPose();
         Pose2d driveBackPose = computeDriveBackPose(scoringPose);
 
         return new DriveToPose(() -> driveBackPose, drivetrain);
@@ -77,13 +78,14 @@ public class AutoScoring {
     }
 
     public Command autoAlgae(Superstructure superstructure){
+        CoralObjective coralObjective = scoringState.getOptimalObjective();
         return new DriveToPose(() -> coralObjective.getScoringPose(), drivetrain)
                 .alongWith(new WaitUntilCommand(() -> closeEnough(coralObjective, Constants.DriverAssistConstants.RAISE_DISTANCE)))
                 .andThen(new DeferredCommand(() -> superstructure.dealgify(coralObjective.getAlgaeLevel()), Set.of(superstructure)));
     }
 
     public Command autoAlgaeNoDrive(Superstructure superstructure){
-        return new DeferredCommand(() -> superstructure.dealgify(coralObjective.getAlgaeLevel()), Set.of(superstructure));
+        return new DeferredCommand(() -> superstructure.dealgify(scoringState.getOptimalObjective().getAlgaeLevel()), Set.of(superstructure));
     }
 
     private static boolean closeEnough(CoralObjective objective, Distance distance){
@@ -92,36 +94,76 @@ public class AutoScoring {
         return currentPose.getTranslation().getDistance(scoringPose.getTranslation()) < distance.in(Meters);
     }
 
-    // Update the reef value used for automatic level selection.
-    public void setBranch(int branchId) {
-        coralObjective = coralObjective.updateBranchId(branchId);
-        System.out.println("Branch ID: " + branchId);
+
+    // allows op to choose level
+    public void overrideLevel(int level) {
+        scoringState = scoringState.updateLevel(level);
     }
 
-    public void setLevel(int level) {
-        coralObjective = coralObjective.updateLevel(level);
+    // removes operator override of level
+    public void removeOverride(){
+        scoringState = scoringState.updateLevel(0);
     }
 
-    public void updateClosestReefPos() {
-        Pose2d[] reefPoses = Constants.DriverAssistConstants.getReefPositions();
-        // Get the current robot pose at initialization.
-        Pose2d currentPose = Swerve.getInstance().getPose();
+    public void addCoral(int branchId, int level){
+        scoringState = scoringState.updateCoral(branchId, level, true);
+    }
 
-        // Find the closest reef pose.
-        double shortestDistance = Double.MAX_VALUE;
-        int closestPoseId = -1;
-        for (int i = 0; i < 12; i++) {
-            Pose2d pose = reefPoses[i];
-            double distance = currentPose.getTranslation().getDistance(pose.getTranslation());
-            if (distance < shortestDistance) {
-                shortestDistance = distance;
-                closestPoseId = i;
-            }
+    public void removeCoral(int branchId, int level){
+        scoringState = scoringState.updateCoral(branchId, level, false);
+    }
+
+    // stores scored coral and current level
+    // if curLevel = 0, it will automatically set it
+    public record ScoringState(boolean[][] corals, int curLevel){
+        public static final ScoringState DEFAULT = new ScoringState(new boolean[4][12], 0);
+
+        public ScoringState updateCoral(int branchId, int level, boolean newValue){
+            corals[level-1][branchId] = newValue;
+            return new ScoringState(corals, curLevel);
         }
-        setBranch(closestPoseId);
 
+        public int getLevel(){
+            if(curLevel == 0){
+                for(int i = 3; i >= 0 ; i--){
+                    for(int j = 0; j < 12; j++){
+                        if(!corals[i][j]){
+                            return i+1;
+                        }
+                    }
+                }
+                return 1;
+            }
+            return curLevel;
+        }
+
+        public ScoringState updateLevel(int newLevel){
+            return new ScoringState(corals, newLevel);
+        }
+
+        public CoralObjective getOptimalObjective(){
+            Pose2d[] reefPoses = Constants.DriverAssistConstants.getReefPositions();
+            Pose2d currentPose = drivetrain.getPose();
+            int level = getLevel();
+
+            // Find the closest reef pose.
+            double shortestDistance = Double.MAX_VALUE;
+            int closestPoseId = -1;
+            for (int i = 0; i < 12; i++) {
+                if(corals[level-1][i]){
+                    continue;
+                }
+                Pose2d pose = reefPoses[i];
+                double distance = currentPose.getTranslation().getDistance(pose.getTranslation());
+                if (distance < shortestDistance) {
+                    shortestDistance = distance;
+                    closestPoseId = i;
+                }
+            }
+            return new CoralObjective(closestPoseId, level);
+        }
     }
-
+    
     // Starting facing the driver station, moving counterclockwise
     public record CoralObjective(int branchId, int level){
         public static final CoralObjective NONE = new CoralObjective(0, 0);
@@ -138,14 +180,6 @@ public class AutoScoring {
         public int getAlgaeLevel(){
             // starting with (0, 1) = 3, then (2, 3) = 2, each reef alternates level between 2 and 3
             return (branchId)/2 % 2 == 0 ? 3 : 2;
-        }
-
-        public CoralObjective updateBranchId(int branchId){
-            return new CoralObjective(branchId, level);
-        }
-
-        public CoralObjective updateLevel(int level){
-            return new CoralObjective(branchId, level);
         }
     }
 }
